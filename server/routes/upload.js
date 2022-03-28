a9abe41bb8c1541ccfe3fcec51feaef8b4372825
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-03-28 21:58:07 +0600

    testing

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -3,7 +3,7 @@ const uploadImage = require("../middleware/uploadImage");
 const auth = require("../middleware/auth");
 
 const router = require("express").Router();
-
+// this is upload route
 router.post("/upload_avatar", uploadImage, auth, uploadAvatar);
 
 module.exports = router;
