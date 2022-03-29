c1347666f108412830e2a66d6e872d734671f761
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-03-29 11:55:04 +0600

    implemeted banner section

--- server/client/src/components/Banner/Banner.jsx
+++ server/client/src/components/Banner/Banner.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Banner = () => {
-  return <section className="banner"></section>;
+  return (
+    <section className="banner">
+      <div className="banner__overlay"></div>
+
+      <div className="banner__content">
+        <h1>Learn Everything You Want</h1>
+        <p>
+          Tecnolgoy is bringing to massive wave of evolution on lerning things
+          on different ways
+        </p>
+        <button>Ready to Get Started ?</button>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
